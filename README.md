e234539207be8bab1855b37f28ca28d7ffb4c2ae
Romain Thibault <rex87@orange.fr>
2020-07-24 23:18:12 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# pythonanywhere_helloworld2
+
+This project is an example of a Python project generated from cookiecutter-python.
+
+## Getting Started
+
+### Prerequisites
+
+*Work in progress ...*
+
+### Installation
+
+*Work in progress ...*
+
+## Usage  
+
+- Double-click **run.bat**.  
+   *Alternatively* execute **run.bat** from the command window:
+   ```
+   run.bat
+   ```
+### Options
+
+- The optional arguments **{options}** can be passed as follows:  
+   ```
+   run.bat {options}
+   ```
+- List of options:
+   ```
+   -h, --help            show this help message and exit
+   -v, --verbose         display debug messages in console
+   ```
+- Show available options:  
+   ```
+   run.bat -h
+   ```
---
+++ pythonanywhere_helloworld2/pythonanywhere_helloworld2.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+	pythonanywhere_helloworld2
+	
+	This project is an example of a Python project generated from cookiecutter-python.
+"""
+
+## -------- COMMAND LINE ARGUMENTS ---------------------------
+## https://docs.python.org/3.7/howto/argparse.html
+import argparse
+CmdLineArgParser = argparse.ArgumentParser()
+CmdLineArgParser.add_argument(
+	"-v",
+	"--verbose",
+	help = "display debug messages in console",
+	action = "store_true",
+)
+CmdLineArgs = CmdLineArgParser.parse_args()
+
+## -------- LOGGING INITIALISATION ---------------------------
+import misc
+misc.MyLoggersObj.SetConsoleVerbosity(ConsoleVerbosity = {True : "DEBUG", False : "INFO"}[CmdLineArgs.verbose])
+LOG, handle_retval_and_log = misc.CreateLogger(__name__)
+
+try:
+	
+	## -------------------------------------------------------
+	## THE MAIN PROGRAM STARTS HERE
+	## -------------------------------------------------------	
+
+	WelcomeText = "Hello pythonanywhere_helloworld2 !"
+
+	# log information
+	LOG.info(WelcomeText)
+	
+	# show message box dialog
+	misc.ShowMessageBox(Text = WelcomeText)
+
+## -------- SOMETHING WENT WRONG -----------------------------	
+except:
+
+	import traceback
+	LOG.error("Something went wrong! Exception details:\n{}".format(traceback.format_exc()))
+
+## -------- GIVE THE USER A CHANCE TO READ MESSAGES-----------
+finally:
+	
+	input("Press any key to exit ...")
